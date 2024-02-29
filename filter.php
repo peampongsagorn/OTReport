2ac5eb71eed8f5fd6ca4c9d86cd0975f8f580a4c
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2024-02-29 09:52:12 +0700

    Edit Button

--- filter.php
+++ filter.php
@@ -70,7 +70,7 @@ if (isset($_POST['submit'])) {
 
 
 <body>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#filterModal">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#filterModal" style="margin-right: 10px">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
             <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
         </svg>
