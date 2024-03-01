87d1e572ce07f5d5156218421218dbcf2fa83f8c
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2024-03-01 11:49:57 +0700

    complete cal 36 hours

--- Chart/chart_36hours.php
+++ Chart/chart_36hours.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('./connection.php');
+require_once('../connection.php');
 
 // เริ่มต้น ตั้งค่าสำหรับเงื่อนไขค้นหา
 $currentYear = date('Y');
@@ -9,130 +9,181 @@ $startYear = $currentYear . '-01-01'; // วันที่ 1 มกราคม
 $filterData = $_SESSION['filter'] ?? null;
 
 // ตั้งค่าเงื่อนไขค้นหาเริ่มต้น
-$sqlConditions_actual = "date BETWEEN '{$startYear}' AND '{$currentDate}'";
-
-$sqlSelect = "weekly_over_times.division_name, COUNT(*) AS OverTime36Count";
-$sqlFrom = "(
-                SELECT 
-                    dv.name AS division_name, 
-                    DATEPART(ISO_WEEK, ot_record.date) as week_num,
-                    SUM(ot_record.attendance_hours) as weekly_hours
-                FROM 
-                    ot_record
-                    INNER JOIN employee e ON ot_record.employee_id = e.employee_id
-                    INNER JOIN costcenter cc ON e.CostcenterID = cc.cost_center_id
-                    INNER JOIN section s ON  cc.section_id = s.section_id
-                    INNER JOIN department d ON s.department_id = d.department_id
-                    INNER JOIN division dv ON d.division_id = dv.division_id
-                    WHERE
-                        $sqlConditions_actual
+$sqlConditions_date = "date BETWEEN '{$startYear}' AND '{$currentDate}'";
+$sqlConditions = "";
+$sql = "SELECT 
+                    dv.name,
+                    COUNT(e.employee_id) AS EmployeesExceeding36Hours
+                FROM (
+                    SELECT 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date) AS WeekNumber, 
+                        SUM(ot.attendance_hours) AS WeeklyHours
+                    FROM 
+                        ot_record ot
+                    WHERE 
+                        {$sqlConditions_date}
+                    GROUP BY 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date)
+                    HAVING 
+                        SUM(ot.attendance_hours) > 36
+                    ) AS WeeklyOT
+                INNER JOIN 
+                    employee e ON WeeklyOT.employee_id = e.employee_id
+                INNER JOIN 
+                    costcenter cc ON e.CostcenterID = cc.cost_center_id
+                INNER JOIN
+                        section s on cc.section_id = s.section_id
+                INNER JOIN 
+                        department d ON s.department_id = d.department_id
+                INNER JOIN
+                        division dv on d.division_id = dv.division_id
                 GROUP BY 
-                    dv.name, 
-                    DATEPART(ISO_WEEK, ot_record.date)
-                HAVING 
-                    SUM(ot_record.attendance_hours) > 36
-            ) AS weekly_over_times";
-$sqlGroupBy = "weekly_over_times.division_name";
+                    dv.name; ";
 
 
 //query เฉลี่ยต่อคนแต่ยังไม่หารวัน
 if ($filterData) {
 
     if (!empty($filterData['startMonthDate']) && !empty($filterData['endMonthDateCurrent'])) {
-        $sqlConditions_actual = "date BETWEEN '{$filterData['startMonthDate']}' AND '{$filterData['endMonthDateCurrent']}'";
+        $sqlConditions_date = "date BETWEEN '{$filterData['startMonthDate']}' AND '{$filterData['endMonthDateCurrent']}'";
     }
 
     if (!empty($filterData['sectionId'])) {
-        $sqlConditions_actual .= " AND cc.section_id = '{$filterData['sectionId']}'";
-        $sqlSelect = "weekly_over_times.cost_center_code, COUNT(*) AS OverTime36CountT";
-        $sqlFrom = "(
-            SELECT 
-                cc.cost_center_code AS cost_center_code, 
-                DATEPART(ISO_WEEK, ot_record.date) as week_num,
-                SUM(ot_record.attendance_hours) as weekly_hours
-            FROM 
-                ot_record
-                INNER JOIN employee e ON ot_record.employee_id = e.employee_id
-                INNER JOIN costcenter cc ON e.CostcenterID = cc.cost_center_id
-                INNER JOIN section s ON  cc.section_id = s.section_id
-                INNER JOIN department d ON s.department_id = d.department_id
-                INNER JOIN division dv ON d.division_id = dv.division_id
+        $sqlConditions = "cc.section_id = '{$filterData['sectionId']}'";
+        $sql = "SELECT 
+                    cc.cost_center_code,
+                    COUNT(DISTINCT e.employee_id) AS EmployeesExceeding36Hours
+                FROM (
+                    SELECT 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date) AS WeekNumber, 
+                        SUM(ot.attendance_hours) AS WeeklyHours
+                    FROM 
+                        ot_record ot
+                    WHERE 
+                        {$sqlConditions_date}
+                    GROUP BY 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date)
+                    HAVING 
+                        SUM(ot.attendance_hours) > 36
+                ) AS WeeklyOT
+                INNER JOIN 
+                    employee e ON WeeklyOT.employee_id = e.employee_id
+                INNER JOIN 
+                    costcenter cc ON e.CostcenterID = cc.cost_center_id
                 WHERE
-                    $sqlConditions_actual
-            GROUP BY 
-                cc.cost_center_code, 
-                DATEPART(ISO_WEEK, ot_record.date)
-            HAVING 
-                SUM(ot_record.attendance_hours) > 36
-        ) AS weekly_over_times";
-        $sqlGroupBy = "weekly_over_times.cost_center_code";
+                    {$sqlConditions}
+                GROUP BY 
+                    cc.cost_center_code;";
 
 
 
     } elseif (!empty($filterData['departmentId'])) {
-        $sqlSelect = "weekly_over_times.section_name, COUNT(*) AS OverTime36Count";
-        $sqlConditions_actual .= " AND s.department_id = '{$filterData['departmentId']}'";
-        $sqlFrom = "(
-            SELECT 
-                s.name AS section_name, 
-                DATEPART(ISO_WEEK, ot_record.date) as week_num,
-                SUM(ot_record.attendance_hours) as weekly_hours
-            FROM 
-                ot_record
-                INNER JOIN employee e ON ot_record.employee_id = e.employee_id
-                INNER JOIN costcenter cc ON e.CostcenterID = cc.cost_center_id
-                INNER JOIN section s ON  cc.section_id = s.section_id
-                INNER JOIN department d ON s.department_id = d.department_id
-                INNER JOIN division dv ON d.division_id = dv.division_id
-                WHERE
-                    $sqlConditions_actual
-            GROUP BY 
-                s.name, 
-                DATEPART(ISO_WEEK, ot_record.date)
-            HAVING 
-                SUM(ot_record.attendance_hours) > 36
-        ) AS weekly_over_times";
-        $sqlGroupBy = "weekly_over_times.section_name";
-
-
+        $sqlConditions= "s.department_id = '{$filterData['departmentId']}'";
+        $sql = "SELECT 
+                    s.name,
+                    COUNT(DISTINCT e.employee_id) AS EmployeesExceeding36Hours
+                FROM (
+                    SELECT 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date) AS WeekNumber, 
+                        SUM(ot.attendance_hours) AS WeeklyHours
+                    FROM 
+                        ot_record ot
+                    WHERE 
+                        {$sqlConditions_date}
+                    GROUP BY 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date)
+                    HAVING 
+                        SUM(ot.attendance_hours) > 36
+                ) AS WeeklyOT
+                INNER JOIN 
+                    employee e ON WeeklyOT.employee_id = e.employee_id
+                INNER JOIN 
+                    costcenter cc ON e.CostcenterID = cc.cost_center_id
+                INNER JOIN
+                    section s on cc.section_id = s.section_id
+                WHERE 
+                    {$sqlConditions}
+                GROUP BY 
+                    s.name;";
 
     } elseif (!empty($filterData['divisionId'])) {
-        $sqlSelect = "weekly_over_times.department_name, COUNT(*) AS OverTime36Count";
-        $sqlConditions_actual .= " AND d.division_id = '{$filterData['divisionId']}'";
-        $sqlFrom = "(
-            SELECT 
-                d.name AS department_name, 
-                DATEPART(ISO_WEEK, ot_record.date) as week_num,
-                SUM(ot_record.attendance_hours) as weekly_hours
-            FROM 
-                ot_record
-                INNER JOIN employee e ON ot_record.employee_id = e.employee_id
-                INNER JOIN costcenter cc ON e.CostcenterID = cc.cost_center_id
-                INNER JOIN section s ON  cc.section_id = s.section_id
-                INNER JOIN department d ON s.department_id = d.department_id
-                INNER JOIN division dv ON d.division_id = dv.division_id
-                WHERE
-                    $sqlConditions_actual
-            GROUP BY 
-                d.name, 
-                DATEPART(ISO_WEEK, ot_record.date)
-            HAVING 
-                SUM(ot_record.attendance_hours) > 36
-        ) AS weekly_over_times";
-        $sqlGroupBy = "weekly_over_times.department_name";
+        $sqlConditions = "d.division_id = '{$filterData['divisionId']}'";
+        $sql = "SELECT 
+                    d.name,
+                    COUNT(e.employee_id) AS EmployeesExceeding36Hours
+                FROM (
+                    SELECT 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date) AS WeekNumber, 
+                        SUM(ot.attendance_hours) AS WeeklyHours
+                    FROM 
+                        ot_record ot
+                    WHERE 
+                        {$sqlConditions_date}
+                    GROUP BY 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date)
+                    HAVING 
+                        SUM(ot.attendance_hours) > 36
+                ) AS WeeklyOT
+                INNER JOIN 
+                    employee e ON WeeklyOT.employee_id = e.employee_id
+                INNER JOIN 
+                    costcenter cc ON e.CostcenterID = cc.cost_center_id
+                INNER JOIN
+                        section s on cc.section_id = s.section_id
+                INNER JOIN 
+                        department d ON s.department_id = d.department_id
+                INNER JOIN
+                        division dv on d.division_id = dv.division_id
+                WHERE 
+                        {$sqlConditions}
+                GROUP BY 
+                    d.name;
+                        ";
+    } else {
+        $sql = "SELECT 
+                    dv.name,
+                    COUNT(e.employee_id) AS EmployeesExceeding36Hours
+                FROM (
+                    SELECT 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date) AS WeekNumber, 
+                        SUM(ot.attendance_hours) AS WeeklyHours
+                    FROM 
+                        ot_record ot
+                    WHERE 
+                        {$sqlConditions_date}
+                    GROUP BY 
+                        ot.employee_id, 
+                        DATEPART(ISO_WEEK, ot.date)
+                    HAVING 
+                        SUM(ot.attendance_hours) > 36
+                    ) AS WeeklyOT
+                INNER JOIN 
+                    employee e ON WeeklyOT.employee_id = e.employee_id
+                INNER JOIN 
+                    costcenter cc ON e.CostcenterID = cc.cost_center_id
+                INNER JOIN
+                        section s on cc.section_id = s.section_id
+                INNER JOIN 
+                        department d ON s.department_id = d.department_id
+                INNER JOIN
+                        division dv on d.division_id = dv.division_id
+                GROUP BY 
+                    dv.name; ";
     }
 }
 
-// คำสั่ง SQL สำหรับการดึงข้อมูล OT
-$sql = "SELECT 
-            $sqlSelect
-        FROM
-            $sqlFrom
-        GROUP BY 
-            $sqlGroupBy";
+
+
             
-
-
 // ดำเนินการ query และเก็บผลลัพธ์
 $stmt = sqlsrv_query($conn, $sql);
 if ($stmt === false) {
@@ -140,21 +191,21 @@ if ($stmt === false) {
 }
 
 // เริ่มต้นตาราง HTML
-echo "<table border='1'>";
-echo "<tr>
-        <th>Division/Department/Section</th>
-        <th>จำนวนครั้งที่ทำ OT เกิน 36 ชม/สัปดาห์</th>
-      </tr>";
 
-// วนลูปผ่านผลลัพธ์ที่ได้จากการ query และแสดงในตาราง
-while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+
+echo "<table border='1'>";
+echo "<tr><th>Division/Section/Department Name</th><th>Employees Exceeding 36 Hours</th></tr>";
+
+// วนลูปผ่านผลลัพธ์ที่ได้จากการ query และแสดงข้อมูลในแต่ละแถวของตาราง
+while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     echo "<tr>";
-    echo "<td>" . htmlspecialchars($row['division_name'] ?? $row['cost_center_code'] ?? $row['section_name'] ?? $row['department_name']) . "</td>"; // ใช้ ?? เพื่อตรวจสอบค่าที่ไม่ได้กำหนด
-    echo "<td>" . htmlspecialchars($row['OverTime36Count']) . "</td>";
+    echo "<td>" . $row['name'] . "</td>"; // แสดงชื่อหน่วยงาน (อาจเป็น Division, Section, หรือ Department ตามเงื่อนไข filter)
+    echo "<td>" . $row['EmployeesExceeding36Hours'] . "</td>"; // แสดงจำนวนพนักงานที่ทำงานเกิน 36 ชั่วโมง
     echo "</tr>";
 }
 
 echo "</table>";
+
 
 
 ?>
