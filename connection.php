2ff73d78cd7d683b0d16c328b8937e6a37a29850
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2024-03-05 09:37:23 +0700

    Connection TO DB Online

--- connection.php
+++ connection.php
@@ -1,20 +1,18 @@
 <?php
-$serverName = "LAPTOP-BD48TPSN\SQLEXPRESS";
+$serverName = "52.139.193.40, 3511"; // แก้ตรงนี้
 $connectionOptions = array(
-    "Database" => "OTReport",
-    "Uid" => "",
-    "PWD" => "",
-    "CharacterSet" => "UTF-8",
- 
+    "Database" => "OTReport", 
+    "Uid" => "follow", 
+    "PWD" => "Follow@2022",
+    "CharacterSet" => "UTF-8"
 );
 
-
+// Establishes the connection เชื่อมต่อ SQL Server
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 if ($conn) {
-    //echo "<script language='javascript'>alert('Connection Successful.');</script>";
+    // echo "<script language='javascript'>alert('Connection Successfull.')
 } else {
     echo "Connection could not be established.";
     die(print_r(sqlsrv_errors(), true));
 }
-?>
